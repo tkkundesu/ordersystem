bf8d66614445c31cd3773226bbe5d26c34faa100
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-11-26 16:36:52 +0900

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 
-define('DSN','mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_eab32af23b1e4b9');
+define('DSN','mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_eab32af23b1e4b9;charset=utf8');
 define('DB_USERNAME','bee96df3ca613a');
 define('DB_PASSWORD','c1716fc1');
 
